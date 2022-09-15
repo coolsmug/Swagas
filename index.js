3c8a7e688f1d6d314f076bc89b46ef37ff0e54c2
Yusuph Idris Olatunji <coolsmug@gmail.com>
2022-09-15 16:00:05 +0100

    changed port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require("path");
 
 
 dotenv.config({ path: "config.env" });
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 const connectDB = require("./server/database/connection");
 connectDB();
 
